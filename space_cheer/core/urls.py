7abe5f2ab47c36ae8d6e98c0620f46dba84e0ccc
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2026-04-08 16:57:41 -0600

    NOMBRE DE LA APP

--- space_cheer/core/urls.py
+++ space_cheer/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from core import views as core_views
 from django.urls import include
 
+app_name = "core"
+
 urlpatterns = [
     path("", core_views.home, name="dashboard"),
 ]
